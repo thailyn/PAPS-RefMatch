bbebbc9cd656b63a8747b601ae4cc61ed3c713d7
Charles Macanka <cmacanka@gmail.com>
2013-03-17 12:36:10 -0400

    Updated run script to include ParseCit include path.
    Also added the "-CSD" flag to allow ParsCit to properly work with Unicode
    text.

--- run_refmatch.sh
+++ run_refmatch.sh
@@ -1,1 +1,1 @@
-DBIC_OVERWRITE_HELPER_METHODS_OK=1 perl -I../PAPS-Database-papsdb-Schema/lib/ refmatch.pl
+DBIC_OVERWRITE_HELPER_METHODS_OK=1 perl -CSD -I../PAPS-Database-papsdb-Schema/lib/ -I../ParsCit/lib/ refmatch.pl
